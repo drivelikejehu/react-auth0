7474b2185adf886a085db29ddf989b450fadc713
drivelikejehu <pollyfactor@hotmail.com>
2020-10-18 03:36:11 -0400

    auth0 callback information on localStorage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Navbar from "./Navbar";
 import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 
 class App extends Component {
   constructor(props) {
@@ -13,20 +14,21 @@ class App extends Component {
   render() {
     return (
       <>
-        <Router>
-          <Navbar />
-          <Switch>
-            <div className="body">
-              {/* <Route path="/" exact component={Home} /> */}
-              <Route
-                path="/"
-                exact
-                render={(props) => <Home auth={this.auth} {...props} />}
-              />
-              <Route path="/profile" component={Profile} />
-            </div>
-          </Switch>
-        </Router>
+        <Navbar />
+
+        <div className="body">
+          {/* <Route path="/" exact component={Home} /> */}
+          <Route
+            path="/"
+            exact
+            render={(props) => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={(props) => <Callback auth={this.auth} {...props} />}
+          />
+          <Route path="/profile" component={Profile} />
+        </div>
       </>
     );
   }
